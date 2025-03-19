7ac568227937f61f325ff7c75de4a4415cb94439
Sudipta Chakraborty <102525583+chakraborty-sudipta@users.noreply.github.com>
2025-03-19 13:50:12 +0530

    Updated with custom DHCP Option Set

--- main.tf
+++ main.tf
@@ -1,36 +1,53 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_vpc" "my-vpc" {
-    cidr_block = "172.16.0.0/16"
-    tags = {
-        Name = "My-VPC"
-    }
+  cidr_block = "172.16.0.0/16"
+  tags = {
+    Name = "My-VPC"
+  }
+}
+
+resource "aws_vpc_dhcp_options" "my_dhcp" {
+  domain_name         = "allianz.com"
+  domain_name_servers = ["AmazonProvidedDNS"]
+  #ntp_servers         = ["172.16.16.16", "10.10.10.10"]
+  #netbios_name_servers = ["192.168.0.4", "198.168.0.5"]
+  #netbios_node_type   = 2
+
+  tags = {
+    Name = "My-DHCP-Options"
+  }
+}
+
+resource "aws_vpc_dhcp_options_association" "my_dhcp_assoc" {
+  vpc_id          = aws_vpc.my-vpc.id
+  dhcp_options_id = aws_vpc_dhcp_options.my_dhcp.id
 }
 
 resource "aws_subnet" "public" {
-    vpc_id = aws_vpc.my-vpc.id
-    cidr_block = "172.16.1.0/24"
-    map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "172.16.1.0/24"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_security_group" "my-sg" {
+  vpc_id = aws_vpc.my-vpc.id 
 
-    vpc_id = aws_vpc.my-vpc.id 
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol ="-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_internet_gateway" "gw" {
@@ -52,27 +69,26 @@ resource "aws_route_table_association" "public_assoc" {
 }
 
 resource "aws_instance" "web-server" {
-    ami = "ami-04b4f1a9cf54c11d0"
-    instance_type = "t2.micro"
-    subnet_id = aws_subnet.public.id
-    security_groups = [aws_security_group.my-sg.id]
-    key_name = "MyKeyPair"
-    tags = {
-        Name = "Web-Server"
-    }
+  ami             = "ami-04b4f1a9cf54c11d0"
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.public.id
+  security_groups = [aws_security_group.my-sg.id]
+  key_name        = "MyKeyPair"
 
-   user_data = <<-EOF
+  tags = {
+    Name = "Web-Server"
+  }
+
+  user_data = <<-EOF
     #!/bin/bash
     sleep 60
     sudo apt update -y
     sudo apt install -y apache2
     sudo systemctl start apache2
     sudo systemctl enable apache2
-EOF
-
+  EOF
 }
 
 output "Web-Server-URL" {
-    value = "http://${aws_instance.web-server.public_ip}"
+  value = "http://${aws_instance.web-server.public_ip}"
 }
-
